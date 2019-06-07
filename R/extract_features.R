980e4ee7f78bee4216bcaa44c9fbc08a69b451dd
Nelson Roque <nelsonroquejr@gmail.com>
2019-06-07 15:23:52 -0400

    maybe its the single quotes tripping up Roxygen...

--- R/extract_features.R
+++ R/extract_features.R
@@ -24,7 +24,7 @@ extract_features <- function(df, group_var, value_var, features='all', custom_fe
   # storing for multiple downstream purposes
   
   if(!data_confirm){
-    #' request acknowledgement from user if they did not pass in the parameter
+    # request acknowledgement from user if they did not pass in the parameter
     
     print("By running this script, you are acknowledging:")
     print("-- your data is in long format")
@@ -34,44 +34,40 @@ extract_features <- function(df, group_var, value_var, features='all', custom_fe
   
   } else {
     
-    #' check if custom feature list provided
+    # check if custom feature list provided
     if(features == "custom") {
       if(is.na(custom_feature_list)){
         stop("ERROR: If 'features' is set to 'custom', you must supply a vector of functions from within the package")
       } else {
-        #' if custom features provided, save into common namespace
+        # if custom features provided, save into common namespace
         current.features <- custom_feature_list
       }
     } else {
-      #' otherwise, get a vector of features based on the provided category
+      # otherwise, get a vector of features based on the provided category
       current.features <- generate_extraction_list(features)
     }
-      
-    #' 
     
-    #' create list to store feature data
+    # create list to store feature data
     feature_list = list()
     
-    #' create dataframe to return feature timing data
+    # create dataframe to return feature timing data
     feature_timing_df = data.frame()
     
-    #' 
-    
-    #' start timer
+    # start timer
     SESSION_START_TIME <- Sys.time()
     SESSION_START_TIMESTAMP = format(SESSION_START_TIME, "%m_%d_%Y-%H_%M_%S")
     
-    #' process all features
+    # process all features
     for(feature_ in current.features) {
         
-      #' start feature timer
+      # start feature timer
       PROCESS_START_TIME = Sys.time()
       PROCESS_START_TIMESTAMP = format(PROCESS_START_TIME, "%m/%d/%Y %H:%M:%S")
             
-      #' extract function name, assuming first argument
+      # extract function name, assuming first argument
       func_name <- feature_[1]
             
-      #' construct function call
+      # construct function call
       if(length(feature_) > 1) {
         func_var_str <- paste(func_name,func_args,sep='_')
         func_args <- feature_[2:length(feature_)]
@@ -82,32 +78,32 @@ extract_features <- function(df, group_var, value_var, features='all', custom_fe
         method_constructor <- paste(func_name,'(',value_var,')')
       }
             
-      #' construct feature method add in support for NAs
+      # construct feature method add in support for NAs
       new_method <- method_constructor
             
-      #' create variable name
+      # create variable name
       new_var_name <- paste0(func_var_str,'_', value_var)
             
-      #' start feature extraction timer
+      # start feature extraction timer
       FEATURE_START_TIME = Sys.time()
       FEATURE_START_TIMESTAMP = format(FEATURE_START_TIME, "%m/%d/%Y %H:%M:%S")
             
-      #' run one feature calculation
+      # run one feature calculation
       df_output <- df %>%
         group_by_(.dots = group_var) %>%
         summarise_(.dots = setNames(new_method, new_var_name))
             
-      #' save each feature as list object
+      # save each feature as list object
       feature_list[[feature_]] <- df_output
             
-      #' end feature timer
+      # end feature timer
       PROCESS_END_TIME = Sys.time()
       
-      #' calculate elapsed time
+      # calculate elapsed time
       feature_elapsed_time = difftime(PROCESS_END_TIME,FEATURE_START_TIME,units='secs')
       process_elapsed_time = difftime(PROCESS_END_TIME,PROCESS_START_TIME,units='secs')
             
-      #' create export dataset with timing features
+      # create export dataset with timing features
       feature_export_df <- data.frame(feature = func_name,
                                       session_start_timestamp = SESSION_START_TIME,
                                       process_start_timestamp = PROCESS_START_TIMESTAMP,
@@ -116,22 +112,22 @@ extract_features <- function(df, group_var, value_var, features='all', custom_fe
                                       feature_start_timer = as.numeric(FEATURE_START_TIME)*1000,
                                       process_end_timer = as.numeric(PROCESS_END_TIME)*1000)
             
-      #' bind to full dataset of timing details
+      # bind to full dataset of timing details
       feature_timing_df <- rbind(feature_timing_df,feature_export_df)
             
-      #' print function name
+      # print function name
       if(verbose) {
         print(paste0("(v.",VERSION_CODE,") ","EXTRACTING: ", func_name," | Elapsed time (secs):",feature_elapsed_time))
       }
     }
         
-    #' print run time
+    # print run time
     ELAPSED_TIME = difftime(Sys.time(),SESSION_START_TIME,units='secs')
     if(verbose) {
       print(paste0("(v.",VERSION_CODE,")"," EXTRACTING: total run time | Elapsed time (secs): ",ELAPSED_TIME))
     }
         
-    #' view timings by feature if desired
+    # view timings by feature if desired
     if(return_timing) {
       TIMING_FN <- paste0("FEATURE_EXTRACTION_SESSION",SESSION_START_TIMESTAMP,".csv")
       write.csv(feature_timing_df,TIMING_FN,row.names=F)
@@ -139,7 +135,7 @@ extract_features <- function(df, group_var, value_var, features='all', custom_fe
       View(feature_timing_df)
     }
     
-    #' return list of features
+    # return list of features
     return(feature_list)
   }
 }
